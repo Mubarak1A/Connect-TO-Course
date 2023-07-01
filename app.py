8af0f558a1526996d7a64f36adac88f14c7ce243
Mubarak <adesinamubarak123@gmail.com>
2023-07-01 10:48:11 +0100

    edit index.html

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ app = Flask (__name__)
 courses = database.load_courses()
 
 @app.route("/")
-def hello_world():
+def index():
     random_courses = functionalties.generate_courses(courses)
     return render_template("index.html",
                            courses=random_courses)
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+
+
 
 
 if __name__ == "__main__":
