921604e8aa8f3ef20a7cc6763426b69886e12309
Mubarak1A <adesinamubarak123@example.com>
2023-07-06 12:11:25 +0100

    temporary edit creds file

---
+++ creds.py
@@ -0,0 +1,10 @@
+#database credential
+username = "mn2oth80xv25qy464gyq"
+passwd = "pscale_pw_4tiRsVvxKieeYFTjnydSL1T9V2D0Tw94EvV95hR9jgn"
+db_host = "aws.connect.psdb.cloud"
+database = "connecttocourse"
+
+
+#api credential
+api_key = "1896963109msh2cb2a05e74add57p10f279jsnd6c70a71e9a0"
+api_host = "udemy-course-scrapper-api.p.rapidapi.com"
