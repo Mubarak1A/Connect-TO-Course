7f34023751947d8ddc716ee36e9b3d840b1de04b
Mubarak Adesina <adesinamubarak123@gmail.com>
2024-03-08 23:55:07 -0800

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,28 @@
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+Base = declarative_base()
 
-class Course:
-    def __init__(self, id, title, url, instructor):
-        self.id = id
-        self.title = title
-        self.url = url
-        self.instructor = instructor
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    instructor = Column(String, nullable=False)
 
     def __str__(self):
         return f"course_id: {self.id}, title: {self.title}, url: {self.url}, instructor: {self.instructor}"
+
+# Db Connection
+
+engine = create_engine("postgresql://your_username:your_password@localhost/your_database")
+Base.metadata.create_all(engine)
