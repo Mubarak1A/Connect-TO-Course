e25ae7bdb86715bde1ff5b9a2f710498a0b9b6a2
Mubarak Adesina <adesinamubarak123@gmail.com>
2024-03-08 23:58:53 -0800

    Update database.py

--- database.py
+++ database.py
@@ -1,55 +1,33 @@
 #!/usr/bin/python3
 """Database module to connect to the cloud database"""
 
-from sqlalchemy import create_engine, text
-import creds
+from sqlalchemy.orm import sessionmaker
+from model import User, Course, engine
 
-db_username = creds.username
-db_password = creds.passwd
-db_host = creds.db_host
-db_name = creds.database
-
-# Change the connection URL for PostgreSQL
-engine = create_engine(f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}")
+# Create a Session class to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
 
 def load_courses():
-    """load all courses from database"""
-    courses = []
-    with engine.connect() as conn:
-        results = conn.execute(text("SELECT * FROM courses"))
-
-        for course in results.fetchall():
-            courses.append(course)
-    return courses
+    """Load all courses from the database"""
+    return session.query(Course).all()
 
 def load_users():
-    """load users from database"""
-    users = []
-    with engine.connect() as conn:
-        results = conn.execute(text("SELECT * FROM users"))
-
-        for user in results.fetchall():
-            users.append(user)
-    return users
-
+    """Load all users from the database"""
+    return session.query(User).all()
 
 def load_bookmark(user_id):
-    """load all user saved courses_id"""
-    with engine.connect() as conn:
-        results = conn.execute(text("SELECT bookmark FROM users WHERE id = {}".format(user_id)))
-    bookmark_courses = results.fetchone()[0]
-    if bookmark_courses:
-        bookmark_ids = [int(id) for id in bookmark_courses.split(" ")]
+    """Load all user saved courses_id"""
+    user = session.query(User).get(user_id)
+    if user:
+        bookmark_ids = [int(id) for id in user.bookmark.split(" ") if id]
     else:
         bookmark_ids = []
-    
-    return (bookmark_ids)
-
+    return bookmark_ids
 
 def load_bookmark_list(user_id):
-    """load the saved courses"""
+    """Load the saved courses"""
     bookmark_ids = load_bookmark(user_id)
-    #load all courses
     all_courses = load_courses()
 
     filtered_courses_list = [
@@ -57,64 +35,52 @@ def load_bookmark_list(user_id):
     ]
     filtered_courses = [
         {
-            'id': course[0],
-            'title': course[1],
-            'url': course[2],
-            'instructor': course[3]
+            'id': course.id,
+            'title': course.title,
+            'url': course.url,
+            'instructor': course.instructor
         }
         for course in filtered_courses_list
     ]
 
-    return (filtered_courses)
-
-    
-
+    return filtered_courses
 
 def save_course(user_id, course_id):
-    """save/bookmark course by storing the id"""
-    bookmark_list = load_bookmark(user_id)
+    """Save/bookmark course by storing the id"""
+    user = session.query(User).get(user_id)
+    if user:
+        bookmark_ids = [int(id) for id in user.bookmark.split(" ") if id]
+    else:
+        bookmark_ids = []
 
-    if course_id not in bookmark_list:
-        bookmark_list.append(course_id)
+    if course_id not in bookmark_ids:
+        bookmark_ids.append(course_id)
 
-    new_bookmark = " ".join(str(id) for id in bookmark_list)
-
-    with engine.connect() as conn2:
-        conn2.execute(text("UPDATE users SET bookmark = '{}' WHERE id = {}".format(new_bookmark, user_id)))
-
-    return 0
-
-
+    user.bookmark = " ".join(str(id) for id in bookmark_ids)
+    session.commit()
 
 def delete_bookmark(user_id, course_id):
     """Delete a particular course id from bookmark"""
-    bookmark_list = load_bookmark(user_id)
+    user = session.query(User).get(user_id)
+    if user:
+        bookmark_ids = [int(id) for id in user.bookmark.split(" ") if id]
+    else:
+        bookmark_ids = []
 
-    if course_id in bookmark_list:
-        bookmark_list.remove(course_id)
-    new_bookmark = " ".join(str(id) for id in bookmark_list)
-    
-    with engine.connect() as conn2:
-        conn2.execute(text("UPDATE users SET bookmark = '{}' WHERE id = {}".format(new_bookmark, user_id)))
+    if course_id in bookmark_ids:
+        bookmark_ids.remove(course_id)
 
-    return 0
-
-
+    user.bookmark = " ".join(str(id) for id in bookmark_ids)
+    session.commit()
 
 def check_user_login(username, passwd):
-    """check if user details in database"""
-    users = load_users()
-
-    for user in users:
-        if user[1] == username and user[3] == passwd:
-            return user[0]
-    else:
-        return False
+    """Check if user details in the database"""
+    user = session.query(User).filter_by(username=username, password=passwd).first()
+    return user.id if user else False
 
 def add_user_details(username, password, email):
-    """add user details to database"""
-    with engine.connect() as conn:
-        query = text("INSERT INTO users (username, email, passwd) VALUES (:username, :email, :passwd)")
-        result = conn.execute(query, {"username": username, "email": email, "passwd": password})
-        print("User Details Added Successfully!")
-
+    """Add user details to the database"""
+    new_user = User(username=username, password=password)
+    session.add(new_user)
+    session.commit()
+    print("User Details Added Successfully!")
